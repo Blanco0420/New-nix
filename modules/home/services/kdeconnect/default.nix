25acad18caba1e340ead9c47136f74f8bea2d889
Blanco0420 <sachajamesterry@gmail.com>
2025-02-08 22:59:50 +0900

    hopefully fixed kde connect

--- modules/home/services/kdeconnect/default.nix
+++ modules/home/services/kdeconnect/default.nix
@@ -1,24 +1,16 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  namespace,
-  ...
-}:
+{ options, config, lib, pkgs, namespace, ... }:
 with lib;
 with lib.custom;
-let
-  cfg = config.services.custom.kdeconnect;
+let cfg = config.services.custom.kdeconnect;
 in {
   options.services.custom.kdeconnect = with types; {
     enable = mkBoolOpt false "Whether or not to manage kdeconnect";
   };
 
   config = mkIf cfg.enable {
-    services.kdeconnect = {
-      enable = true;
-      indicator = true;
+      services.kdeconnect = {
+        enable = true;
+        # indicator = true;
+      };
     };
-  };
-}
+  }
